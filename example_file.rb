cca452d742bb1a9a1bbba207d00d1f6bba49aee7
Sanny Do <sanny.do94@gmail.com>
2021-01-29 02:36:31 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,13 @@
+english_music_by_city = {
+  :manchester => [
+    {
+      :band_name => "The_Smiths",
+      :member_names => ["Andy", "Mike"]
+    },
+    {
+      :band_name => "Joy Division",
+      :member_naes => ["Peter", "John"]
+    },
+  ]
+}
+
---
+++ greeting.rb
@@ -0,0 +1,14 @@
+def greeting
+  puts "Hello World"
+end
+
+def say_greeting_five_times
+  
+#code below is to call 
+greeting
+greeting
+greeting
+greeting
+end
+
+say_greeting_five_times
